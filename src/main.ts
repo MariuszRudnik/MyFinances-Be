2cecf225daab557d85720fecd7792233ba651946
MariuszRudnik <86014393+MariuszRudnik@users.noreply.github.com>
2023-05-03 16:27:16 +0200

    credentials: false,

--- src/main.ts
+++ src/main.ts
@@ -12,7 +12,7 @@ async function bootstrap() {
   app.useGlobalPipes(new ValidationPipe());
   app.use(cookieParser());
   app.enableCors({
-    credentials: true,
+    credentials: false,
     origin: [],
   });
 
