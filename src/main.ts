dd232fd749911aa98e6a9ea96795e156e97f40dd
MariuszRudnik <86014393+MariuszRudnik@users.noreply.github.com>
2023-09-02 18:40:31 +0200

    edit cors 3

--- src/main.ts
+++ src/main.ts
@@ -12,8 +12,8 @@ async function bootstrap() {
   app.useGlobalPipes(new ValidationPipe());
   app.use(cookieParser());
   app.enableCors(
-      {origin: 'http://localhost:3000',
-
+      {
+        origin: ['http://localhost:3000', 'https://my-finance-fe.vercel.app']
   });
 
   const config = new DocumentBuilder()
